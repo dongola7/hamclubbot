2d90322c94dbfe76447c4c01d7fc8c34addfeef3
Blair Kitchen <blair@blairkitchen.com>
2025-09-13 14:43:06 -0400

    Adding the /muf command to produce a MUF map via embed

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import os
 import io
 import discord
 import webcache
+import cairosvg
 
 dotenv.load_dotenv('config.env')
 bot = discord.Bot()
@@ -24,6 +25,29 @@ async def conditions(ctx: discord.ApplicationContext):
             description="Images from [hamqsl.com](https://www.hamqsl.com)"
         )
         embed.set_image(url="attachment://conditions.jpg")
+
+        await ctx.respond(embed=embed, file=file)
+
+@bot.slash_command(name="muf", description="Show current MUF map from https://prop.kc2g.com")
+async def muf(ctx: discord.ApplicationContext):
+    URL = 'https://prop.kc2g.com/renders/current/mufd-normal-now.svg'
+    cache_entry = cache.getUrl(URL)
+
+    # If there is no extra data associated with the cache, then we need to
+    # convert the SVG to a PNG and cache the PNG value
+    if 'extra' not in cache_entry:
+        png_bytes = cairosvg.svg2png(bytestring = cache_entry['content'])
+        cache.cacheRelatedData(URL, png_bytes)
+    else:
+        png_bytes = cache_entry['extra']
+
+    with io.BytesIO(png_bytes) as content:
+        file = discord.File(fp = content, filename = 'mufmap.png')
+        embed = discord.Embed(
+            title = "Current MUF Map",
+            description="Map from [prop.kc2g.com](https://prop.kc2g.com)"
+        )
+        embed.set_image(url="attachment://mufmap.png")
 
         await ctx.respond(embed=embed, file=file)
 
