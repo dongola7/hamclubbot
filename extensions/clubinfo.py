f52fdd14a1983d481a5390c9bf3266199fc4bd22
Blair Kitchen <blair@blairkitchen.com>
2025-09-14 14:15:52 -0400

    Implementing dynamic club info via discord autocomplete

--- extensions/clubinfo.py
+++ extensions/clubinfo.py
@@ -5,31 +5,36 @@ import extensions.basecog
 logger = logging.getLogger(__name__)
 
 class ClubInfo(extensions.basecog.Cog):
-    """Implements commands providing club information"""
+    """
+    Implements commands providing club information
 
-    cmd_group = discord.SlashCommandGroup(name="club", description="Provides club information")
+    This Cog provides a command 'club' for members to request more information about various
+    aspects of the club. The Cog uses the Discord autocomplete feature to produce a dynamic
+    list of possible queries based on the bots configuration. This allows the configuration
+    to be updated to add/remove information without modifying the code itself.
+    """
 
     def __init__(self, bot: discord.Bot):
         super().__init__(bot, config_name='clubInfo')
-        
-    @cmd_group.command(name="nets", description="Information about club nets")
-    async def nets(self, ctx: discord.ApplicationContext):
-        embed = self._generate_embed("nets")
-        await ctx.respond(embed=embed)
+
+        self.__subcommands = []
+        for command in self.config:
+            self.__subcommands.append(command)
     
-    @cmd_group.command(name="meetings", description="Information about club meetings")
-    async def meetings(self, ctx: discord.ApplicationContext):
-        embed = self._generate_embed("meetings")
-        await ctx.respond(embed=embed)
-    
-    @cmd_group.command(name="repeaters", description="Information about club repeaters")
-    async def repeaters(self, ctx: discord.ApplicationContext):
-        embed = self._generate_embed("repeaters")
+    def get_subcommands(self, ctx: discord.AutocompleteContext):
+        return self.__subcommands
+            
+    @discord.command(name="club", description="Provides club information")
+    @discord.option(name="what", description="What do you want to know about?", autocomplete=get_subcommands)
+    async def club(self, 
+                   ctx: discord.ApplicationContext, 
+                   what: str):
+        embed = self._generate_embed(what)
         await ctx.respond(embed=embed)
     
     def _generate_embed(self, info_type: str) -> discord.Embed:
         if info_type not in self.config:
-            content = f"I don't have any information about club {info_type}. Ask my owner to add some!"
+            content = f"I don't have any information about '{info_type}'. Ask my owner to add some!"
             return self._embed(description=content)
         
         # If the config is just a simple string, then it should point to a file
