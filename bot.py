5a8a79e669c4c5091b5afb1d1a545635832c7754
Blair Kitchen <blair@blairkitchen.com>
2025-09-13 20:27:10 -0400

    Refactoring code to get ready for further development
    * Separating the conditions commands into an extension
    * Moving config params from an environment file to a JSON file

--- bot.py
+++ bot.py
@@ -2,53 +2,42 @@
 
 import dotenv
 import os
-import io
 import discord
-import webcache
-import cairosvg
+import logging
+import json
+import time
 
-dotenv.load_dotenv('config.env')
+# Create logger
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)-8s %(name)s : %(message)s")
+logger = logging.getLogger("simple-bot")
+
+# Load config file
+CONFIG_PATH = os.getenv("BOT_CONFIG", "./config.json")
+try:
+    with open(CONFIG_PATH, "r") as config_stream:
+        logger.info(f'Loading config from {CONFIG_PATH}')
+        config = json.load(config_stream)
+        logger.info("successfully loaded config")
+except FileNotFoundError:
+    logger.error(f"Config file not found at {CONFIG_PATH}")
+    raise SystemExit(1)
+except Exception as ex:
+    logger.error(f"Failed to load config from {CONFIG_PATH}: {ex}")
+    raise SystemExit(1)
+
+# Create the bot instance
 bot = discord.Bot()
-cache = webcache.WebCache()
+bot.start_time = time.time()
+bot.config = config
+
+# Load extensions
+bot.load_extension("extensions.conditions")
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} is ready and online!")
+    logger.info(f"Username: {bot.user}")
+    logger.info(f"Servers: {len(bot.guilds)}")
+    logger.info("bot ready...")
 
-@bot.slash_command(name="conditions", description="Show current conditions from https://hamqsl.com")
-async def conditions(ctx: discord.ApplicationContext):
-    cache_entry = cache.getUrl('https://www.hamqsl.com/solar101pic.php')
-    with io.BytesIO(cache_entry['content']) as content:
-        file = discord.File(fp = content, filename='conditions.jpg')
-        embed = discord.Embed(
-            title = "Current Solar Conditions",
-            description="Images from [hamqsl.com](https://www.hamqsl.com)"
-        )
-        embed.set_image(url="attachment://conditions.jpg")
-
-        await ctx.respond(embed=embed, file=file)
-
-@bot.slash_command(name="muf", description="Show current MUF map from https://prop.kc2g.com")
-async def muf(ctx: discord.ApplicationContext):
-    URL = 'https://prop.kc2g.com/renders/current/mufd-normal-now.svg'
-    cache_entry = cache.getUrl(URL)
-
-    # If there is no extra data associated with the cache, then we need to
-    # convert the SVG to a PNG and cache the PNG value
-    if 'extra' not in cache_entry:
-        png_bytes = cairosvg.svg2png(bytestring = cache_entry['content'])
-        cache.cacheRelatedData(URL, png_bytes)
-    else:
-        png_bytes = cache_entry['extra']
-
-    with io.BytesIO(png_bytes) as content:
-        file = discord.File(fp = content, filename = 'mufmap.png')
-        embed = discord.Embed(
-            title = "Current MUF Map",
-            description="Map from [prop.kc2g.com](https://prop.kc2g.com)"
-        )
-        embed.set_image(url="attachment://mufmap.png")
-
-        await ctx.respond(embed=embed, file=file)
-
-bot.run(os.getenv('DISCORD_TOKEN'))
+bot.run(config['discordToken'])
---
+++ extensions/conditions.py
@@ -0,0 +1,48 @@
+import discord
+import webcache
+import io
+import cairosvg
+
+class Conditions(discord.Cog):
+    """Provides a set of discord bot commands for checking radio weather conditions"""
+    def __init__(self):
+        self.__cache = webcache.WebCache()
+
+    @discord.command(name="cond", description="Show current conditions from https://hamqsl.com")
+    async def cond(self, ctx: discord.ApplicationContext):
+        cache_entry = self.__cache.getUrl('https://www.hamqsl.com/solar101pic.php')
+        with io.BytesIO(cache_entry['content']) as content:
+            file = discord.File(fp = content, filename='conditions.jpg')
+            embed = discord.Embed(
+                title = "Current Solar Conditions",
+                description="Images from [hamqsl.com](https://www.hamqsl.com)"
+            )
+            embed.set_image(url="attachment://conditions.jpg")
+
+            await ctx.respond(embed=embed, file=file)
+
+    @discord.command(name="muf", description="Show current MUF map from https://prop.kc2g.com")
+    async def muf(self, ctx: discord.ApplicationContext):
+        URL = 'https://prop.kc2g.com/renders/current/mufd-normal-now.svg'
+        cache_entry = self.__cache.getUrl(URL)
+
+        # If there is no extra data associated with the cache, then we need to
+        # convert the SVG to a PNG and cache the PNG value
+        if 'extra' not in cache_entry:
+            png_bytes = cairosvg.svg2png(bytestring = cache_entry['content'])
+            self.__cache.cacheRelatedData(URL, png_bytes)
+        else:
+            png_bytes = cache_entry['extra']
+
+        with io.BytesIO(png_bytes) as content:
+            file = discord.File(fp = content, filename = 'mufmap.png')
+            embed = discord.Embed(
+                title = "Current MUF Map",
+                description="Map from [prop.kc2g.com](https://prop.kc2g.com)"
+            )
+            embed.set_image(url="attachment://mufmap.png")
+
+            await ctx.respond(embed=embed, file=file)
+
+def setup(bot: discord.Bot):
+    bot.add_cog(Conditions())
