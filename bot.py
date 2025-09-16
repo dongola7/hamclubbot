5e7b7ed65c64826a51bcf4ea55540b11364f5b12
Blair Kitchen <blair@blairkitchen.com>
2025-09-15 21:45:04 -0400

    Adding ability to configure logging through the config file

--- bot.py
+++ bot.py
@@ -1,29 +1,38 @@
 #!python3
-
-import os
 import extensions.util.simplebot as simplebot
 import logging
+import logging.config
 import yaml
 import discord
+import argparse
 
-# Create logger
-logging.basicConfig(level=logging.INFO,
-                    format="%(asctime)s %(levelname)-8s %(name)s : %(message)s")
-logger = logging.getLogger("bot")
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="Discord bot for use in ham radio club discords")
+parser.add_argument("-c", "--config", required=True, help="Config file location")
+args = parser.parse_args()
 
 # Load config file
-CONFIG_PATH = os.getenv("BOT_CONFIG", "./config.yaml")
 try:
-    with open(CONFIG_PATH, "r") as config_stream:
-        logger.info(f'Loading config from {CONFIG_PATH}')
+    with open(args.config, "r") as config_stream:
         config = yaml.safe_load(config_stream)
-        logger.info("successfully loaded config")
 except FileNotFoundError:
-    logger.error(f"Config file not found at {CONFIG_PATH}")
-    raise SystemExit(1)
+    raise SystemExit(f"Config file not found at {args.config}")
 except Exception as ex:
-    logger.error(f"Failed to load config from {CONFIG_PATH}: {ex}")
-    raise SystemExit(1)
+    raise SystemExit(f"Failed to load config from {args.config}: {ex}")
+
+# Configure logging if present in config
+if 'logging' in config:
+    config['logging']['version'] = 1
+    config['logging']['incremental'] = False
+    logging.config.dictConfig(config['logging'])
+else:
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)-8s %(name)s : %(message)s")
+
+# Create the logger
+logger = logging.getLogger("bot")
+
 
 # Create the bot instance
 bot = simplebot.SimpleBot(config)
