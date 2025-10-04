11d6b58146e138372e00b092bfe963f278865c5e
Blair Kitchen <blair@blairkitchen.com>
2025-10-04 12:06:40 -0400

    Fixing linting errors in extensions/util/views

--- extensions/util/views.py
+++ extensions/util/views.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2025, Blair Kitchen
+# All rights reserved.
+#
+# See the file LICENSE for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+
+"""Implements common views for use in the discord bot"""
+
 import discord
 
 class YesNoConfirmationView(discord.ui.View):
@@ -8,14 +16,17 @@ class YesNoConfirmationView(discord.ui.View):
 
     @property
     def selection(self) -> str | None:
+        """Returns the selection made by the user. None if no selection made before timeout"""
         return self.__selection
 
     @discord.ui.button(label="Yes")
-    async def yes_callback(self, button: discord.Button, interaction: discord.Interaction):
+    async def yes_callback(self, _button: discord.Button, _interaction: discord.Interaction):
+        """Callback for handling the 'yes' button click"""
         self.__selection = "yes"
         self.stop()
 
     @discord.ui.button(label="No")
-    async def no_callback(self, button: discord.Button, interaction: discord.Interaction):
+    async def no_callback(self, _button: discord.Button, _interaction: discord.Interaction):
+        """Callback for handling the 'yes' button click"""
         self.__selection = "no"
         self.stop()
