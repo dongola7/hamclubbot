b007ba0ad13f2f8d4e8353709646bdf197f20143
Blair Kitchen <blair@blairkitchen.com>
2025-10-04 12:01:59 -0400

    Fixing linting errors in extensions/util/persistentstore

--- extensions/util/persistentstore.py
+++ extensions/util/persistentstore.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2025, Blair Kitchen
+# All rights reserved.
+#
+# See the file LICENSE for information on usage and redistribution
+# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+
+"""Implements a basic persistent storage system"""
+
 import sqlite3
 import logging
 
@@ -18,37 +26,47 @@ class PersistentGuildStore:
             cursor = conn.execute("PRAGMA USER_VERSION")
 
             if cursor.fetchone()[0] == 0:
-                logger.info(f"initializing persistent storage at {dbpath}")
-                cursor.execute("CREATE TABLE storage (guild_id INTEGER, key TEXT, value TEXT, PRIMARY KEY (guild_id, key))")
+                logger.info("initializing persistent storage at %s", dbpath)
+                cursor.execute("CREATE TABLE storage (\
+                                guild_id INTEGER, key TEXT, value TEXT,\
+                                PRIMARY KEY (guild_id, key))")
                 cursor.execute("PRAGMA USER_VERSION=1")
                 cursor.close()
-                logger.info(f"successfully initialized persistent storage at {dbpath}")
+                logger.info("successfully initialized persistent storage at %s", dbpath)
 
     def get_value(self, key: str, default: str | None = None) -> str | None:
+        """Gets the value for the specified key from the store"""
         with sqlite3.connect(self.__dbpath) as conn:
-            cursor = conn.execute("SELECT value FROM storage WHERE guild_id=? AND key=?", (self.__guild_id, key,))
+            cursor = conn.execute("SELECT value FROM storage WHERE guild_id=? AND key=?",
+                (self.__guild_id, key,))
             row = cursor.fetchone()
-            if row == None:
+            if row is None:
                 return default
-            else:
-                return row[0]
+            return row[0]
 
     def set_value(self, key: str, value: str):
+        """Persistently stores the given value for the specified key"""
         with sqlite3.connect(self.__dbpath) as conn:
-            conn.execute("INSERT INTO storage (guild_id, key, value) VALUES(?, ?, ?) ON CONFLICT(guild_id, key) DO UPDATE SET value=?",
+            conn.execute("INSERT INTO storage (guild_id, key, value) VALUES(?, ?, ?)\
+                          ON CONFLICT(guild_id, key) DO UPDATE SET value=?",
                         (self.__guild_id, key, value, value))
 
     def delete_value(self, key: str):
+        """Deletes the key from the persistent store"""
         with sqlite3.connect(self.__dbpath) as conn:
             conn.execute("DELETE FROM storage WHERE guild_id=? AND key=?", (self.__guild_id, key,))
 
     def get_keys(self, prefix: str | None = None):
+        """Gets all keys matching the specified prefix"""
         with sqlite3.connect(self.__dbpath) as conn:
             if prefix:
-                filter=f"{prefix}%"
-                cursor = conn.execute("SELECT key FROM storage WHERE guild_id=? AND key LIKE ? ORDER BY KEY ASC", (self.__guild_id, filter))
+                comparison=f"{prefix}%"
+                cursor = conn.execute("SELECT key FROM storage WHERE guild_id=?\
+                                      AND key LIKE ? ORDER BY KEY ASC",
+                                      (self.__guild_id, comparison))
             else:
-                cursor = conn.execute("SELECT key FROM storage WHERE guild_id=? ORDER BY KEY ASC", (self.__guild_id,))
+                cursor = conn.execute("SELECT key FROM storage WHERE guild_id=?\
+                                       ORDER BY KEY ASC", (self.__guild_id,))
 
             result = []
             for row in cursor:
