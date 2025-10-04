c7772af8cc5ccb96f7844340adc7a51597daf6f7
Blair Kitchen <blair@blairkitchen.com>
2025-10-04 14:48:16 -0400

    Fixing linting errors in extensions.pota

--- extensions/pota.py
+++ extensions/pota.py
@@ -4,15 +4,18 @@
 # See the file LICENSE for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 
-import discord
-import json
-import urllib.parse
-import logging
-import extensions.util.simplebot as simplebot
-import extensions.util.webcache as webcache
-import asyncio
+"""Extension implementing a Cog to lookup pota information"""
+
 from typing import cast
+import json
+import asyncio
 import io
+import logging
+import urllib.parse
+
+import discord
+
+from extensions.util import simplebot, webcache
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +28,29 @@ class Pota(simplebot.SimpleCog):
         super().__init__(bot)
         self.__cache = webcache.WebCache()
 
-    cmd_group = discord.SlashCommandGroup(name="pota", description="Query the pota.app website for details")
+    cmd_group = discord.SlashCommandGroup(name="pota",
+        description="Query the pota.app website for details")
+
+    def _format_park_name(self, park_info):
+        quoted_park = urllib.parse.quote(park_info['reference'])
+        link = f"https://pota.app/#/park/{quoted_park}"
+        return f"[{park_info['reference']}]({link}) - [{park_info['name']}, \
+{park_info['locationName']}]({park_info['website']})"
+
+    def _format_park_stats(self, park_stats):
+        return f"{park_stats['activations']}/{park_stats['attempts']} activations, \
+{park_stats['contacts']} QSOs"
+
+    def _format_recent_activations(self, activations):
+        with io.StringIO() as result:
+            for a in activations:
+                date = str(a['qso_date'])
+                date_str = f"{date[0:4]}-{date[4:6]}-{date[6:8]}"
+                print(f"* {date_str} - **{a['activeCallsign']}** - {a['totalQSOs']} QSOs",
+                    file=result)
+                print(f"  * {a['qsosCW']} CW, {a['qsosDATA']} Data, {a['qsosPHONE']} Phone",
+                    file=result)
+            return result.getvalue()
 
     @cmd_group.command(name="activations", description="Get recent activations for a specific park")
     @discord.option(name="park", description="The park number (e.g. US-8081)")
@@ -33,46 +58,44 @@ class Pota(simplebot.SimpleCog):
         """Responds with recent activations for the given park."""
         park = park.upper()
         quoted_park = urllib.parse.quote(park)
-        stats_url = f"https://api.pota.app/park/stats/{quoted_park}"
-        info_url = f"https://api.pota.app/park/{quoted_park}"
-        recent_url=f"https://api.pota.app/park/activations/{quoted_park}?count=5"
 
         caches = await asyncio.gather(
-            self.__cache.get_url(stats_url),
-            self.__cache.get_url(info_url),
-            self.__cache.get_url(recent_url)
+            self.__cache.get_url(f"https://api.pota.app/park/stats/{quoted_park}"),
+            self.__cache.get_url(f"https://api.pota.app/park/{quoted_park}"),
+            self.__cache.get_url(f"https://api.pota.app/park/activations/{quoted_park}?count=5")
         )
-        stats_cache, info_cache, recent_cache = map(lambda o: cast(webcache.CacheEntry, o), caches)
-        stats_result, info_result, recent_result = map(lambda c: json.loads(c.content), [stats_cache, info_cache, recent_cache])
+        stats_cache, info_cache, recent_cache = map(
+            lambda o: cast(webcache.CacheEntry, o), caches)
+        stats_result, info_result, recent_result = map(
+            lambda c: json.loads(c.content), [stats_cache, info_cache, recent_cache])
 
         # Check for error messages
         if isinstance(stats_result, str):
-            await ctx.respond(f"error while querying the pota website for park {park}: {stats_result}", ephemeral=True)
+            await ctx.respond(
+                f"error while querying the pota website for park {park}: {stats_result}",
+                ephemeral=True)
         elif isinstance(info_result, str):
-            await ctx.respond(f"error while querying the pota website for park {park}: {info_result}", ephemeral=True)
+            await ctx.respond(
+                f"error while querying the pota website for park {park}: {info_result}",
+                ephemeral=True)
         elif isinstance(recent_result, str):
-            await ctx.respond(f"error while querying the pota website for park {park}: {recent_result}", ephemeral=True)
+            await ctx.respond(
+                f"error while querying the pota website for park {park}: {recent_result}",
+                ephemeral=True)
         else:
-            link = f"https://pota.app/#/park/{quoted_park}"
             embed = self._embed(title=f"{park} Stats",
-                                description=f"Information provided by [pota.app](https://pota.app).")
+                description="Information provided by [pota.app](https://pota.app).")
             embed.set_footer(text=recent_cache.last_refreshed_str())
 
             embed.add_field(name="Park Name",
-                            value=f"[{park}]({link}) - [{info_result['name']}, {info_result['locationName']}]({info_result['website']})",
-                            inline=False)
+                value=self._format_park_name(info_result),
+                inline=False)
             embed.add_field(name="Stats",
-                            value=f"{stats_result['activations']}/{stats_result['attempts']} activations, {stats_result['contacts']} QSOs",
-                            inline=False)
-
-            with io.StringIO() as activations:
-                for entry in recent_result:
-                    date = str(entry['qso_date'])
-                    date_str = f"{date[0:4]}-{date[4:6]}-{date[6:8]}"
-                    print(f"* {date_str} - **{entry['activeCallsign']}** - {entry['totalQSOs']} QSOs", file=activations)
-                    print(f"  * {entry['qsosCW']} CW, {entry['qsosDATA']} Data, {entry['qsosPHONE']} Phone", file=activations)
-
-                embed.add_field(name="Recent Activations", value=activations.getvalue())
+                value=self._format_park_stats(stats_result),
+                inline=False)
+            embed.add_field(name="Recent Activations",
+                value=self._format_recent_activations(recent_result),
+                inline=False)
 
             await ctx.respond(embed=embed)
 
@@ -83,21 +106,25 @@ class Pota(simplebot.SimpleCog):
         callsign = callsign.upper()
         url = f"https://api.pota.app/stats/user/{urllib.parse.quote(callsign)}"
         cache_entry = await self.__cache.get_url(url)
-        logger.debug(f"queried {url} and received {cache_entry.content}")
+        logger.debug("queried %s and received %s", url, cache_entry.content)
 
         result = json.loads(cache_entry.content)
 
         if isinstance(result, str):
             # Error message from the pota API
-            await ctx.respond(f"error while querying the pota website for callsign {callsign}: {result}", ephemeral=True)
+            await ctx.respond(
+                f"error while querying the pota website for callsign {callsign}: {result}",
+                ephemeral=True)
         else:
             link = f"https://pota.app/#/profile/{urllib.parse.quote(callsign)}"
             embed = self._embed(title=f"{callsign}'s POTA Stats",
-                                description=f"Information provided by [pota.app](https://pota.app).\n\nSee [{callsign}'s profile]({link}) for details.")
+                description=f"Information provided by [pota.app](https://pota.app).\
+\n\nSee [{callsign}'s profile]({link}) for details.")
 
             activator = result['activator']
             attempts = result['attempts']
-            activator_desc = f"{activator['activations']}/{attempts['activations']} activations in {activator['parks']}/{attempts['parks']} parks. {activator['qsos']} total QSOs."
+            activator_desc = f"{activator['activations']}/{attempts['activations']} activations in\
+{activator['parks']}/{attempts['parks']} parks. {activator['qsos']} total QSOs."
             embed.add_field(name="Activator", value=activator_desc, inline=False)
 
             hunter = result['hunter']
@@ -109,4 +136,5 @@ class Pota(simplebot.SimpleCog):
             await ctx.respond(embed=embed)
 
 def setup(bot: simplebot.SimpleBot):
+    """Called when the extension is loaded"""
     bot.add_cog(Pota(bot))
